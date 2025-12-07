78ad43b4ccf837210b5a8bb019ddbfcb29a3d03f
Kyle Hale <kylehale343@gmail.com>
2025-12-07 14:15:35 -0700

    adding project mvp (#1)
    Co-authored-by: Kyle Hale <LL001536@rn-office.com>

---
+++ models.go
@@ -0,0 +1,44 @@
+package main
+
+import "database/sql"
+
+type App struct {
+	DB *sql.DB
+}
+
+type Track struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Car       string `json:"car"`
+	IsActive  bool   `json:"is_active"`
+	CreatedAt string `json:"created_at"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	CreatedAt string `json:"created_at"`
+}
+
+type LapTime struct {
+	ID               int    `json:"id"`
+	TrackID          int    `json:"track_id"`
+	UserID           int    `json:"user_id"`
+	Username         string `json:"username"`
+	Time             string `json:"time"`
+	ABS              bool   `json:"abs"`
+	AutoTransmission bool   `json:"auto_transmission"`
+	TractionControl  bool   `json:"traction_control"`
+	CreatedAt        string `json:"created_at"`
+}
+
+type LeaderboardEntry struct {
+	UserID           int    `json:"user_id"`
+	Username         string `json:"username"`
+	Time             string `json:"time"`
+	Position         int    `json:"position"`
+	ABS              bool   `json:"abs"`
+	AutoTransmission bool   `json:"auto_transmission"`
+	TractionControl  bool   `json:"traction_control"`
+	CreatedAt        string `json:"created_at"`
+}
